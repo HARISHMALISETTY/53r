01ce074ce994693401f0fdc33017ff0129d85f1e
HARISHMALISETTY <malisettyharish@gmail.com>
2025-06-19 14:53:10 +0530

    inclass-notes

---
+++ day1/datatypes.py
@@ -0,0 +1,36 @@
+# x=5 #integer
+# y=12.5 #float
+# z="something" #string
+# a=3+8j
+# is_available=True
+
+# value1=25
+# print(id(value1))
+# value1=40
+# print(id(value1))
+
+# print(value1)
+
+# list--->[]
+#tuples---->()
+#set---->{}
+#dictionary--->{}
+
+fruits=["apple","mango","banana"] # collection of strings and list of fruits
+ids=[1,2,3,4] #collection of integers and list of ids
+prices=[125.67,256.2,450.2,116.22] #collection of floatingvalues and list of prices
+mixed=[1,2,"hi",12.5,True]
+# print(type(fruits))
+# print(type(ids))
+# print(type(prices))
+# print(type(mixed))
+
+#so we can say a list can be a collection of homogeneous / heterogeneous elements.
+
+print(len(fruits))
+
+#length of the fruits collection is 3
+
+# print(f"length of the fruits collection is {len(fruits)}")
+
+print(fruits[len(fruits)-1]) 
---
+++ day1/sample.py
@@ -0,0 +1,28 @@
+
+x="500"
+y="500"
+print(id(x))
+#print(id(y))
+line="""jhhjhjhjghvj
+hbbb, m, m, 
+"m mn nm nm"""
+
+
+# name="kartik"
+# city="bnglr"
+
+#i want to print harish is working in hyd
+
+# print(name,"is working in",city)
+# print(f"{name} is working in {city}")
+
+# student="krishna"
+# course="python"
+# city="hyd"
+
+student=input("enter student name:")
+course=input("enter course name:")
+city=input("enter city name:")
+#jeswanth joined python course in hyd
+
+print(f"{student} joined {course} course in {city}")
