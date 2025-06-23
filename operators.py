3f2c6a18cfbd4a4b2ae50eddc90f62ca9c62d18c
HARISHMALISETTY <malisettyharish@gmail.com>
2025-06-23 18:18:41 +0530

    operators

---
+++ operators.py
@@ -0,0 +1,131 @@
+# a=5
+# b=6
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)--->quotient
+# print(40%9)--->reminder
+# print(20//3)---->gives quotient by ignoring floating points
+# print(a**b)
+
+#comparision
+# 1.==
+# a=5
+# b=5
+# print(a==b)
+
+input_password="strongpasswords"
+db_stored_password="strongpassword"
+
+# print(input_password==db_stored_password)
+# print(input_password!=db_stored_password)
+
+a=10
+b=5
+
+
+# print(a>b)
+
+quantity_in_store=25
+client_require=35
+
+# print(quantity_in_store>client_require) #it will prints false
+# print(quantity_in_store<client_require) #it will prints true
+
+# print(10>=10) # true
+# print(10<=8) # false
+
+
+# str1="welcome"
+# str2="something"
+
+# print(len(str1)>len(str2))
+
+# list1=[1,2,3,4,5,6]
+# list2=[10,11,12,4,6]
+
+# print(len(list1)!=len(list2))
+
+#i want to check whether 3rd element in first list is greater than 4th element in the second list
+
+# print(list1[2]>list2[3])
+
+#logical and ---> and
+# logical or----> or
+#logical not---->not
+
+# a=5
+# b=3
+
+#print(True and False) #gives false
+# print(a>2 and b>5) #gives false
+# print(True or False) #gives true
+# print(a>2 or b>5) #gives true
+#print(True and True)
+#print(True or True) gives True
+#print(a<10 and b<5) #gives true
+#print(False and False)
+#print(False or False) #gives false
+#print(a>10 and b>5) #gives false
+# print(False and True)
+# print(False or True)
+# print(not False)
+
+
+
+# a=a+2 is same as a+=2
+
+# a=a+2
+# a+=2 # addition assignment operator 
+# a=a-2
+# a-=2 #subtrcion and assignment
+
+# a=a*2 #here a will becomes 10
+# a=5
+# a*=2 #10
+# a+=5 #15
+# a-=2 #13 
+# a*=6 #78
+# a=a/2 +5 #44
+# a/=5 #8.8
+# # a%=2
+# a//=4 #2
+# a**=5
+# print(a)
+
+
+
+# str="hello world"
+
+# op1="k" in str # k is not a member in str here, so it will give false.
+# print(op1)
+
+# list1=[4,5,8,9,12,13]
+
+# # print(9 in list1)
+# op=9 in list1
+# print(op)
+
+# tuple1=(5,8,7,6,10)
+# op=18 in tuple1
+# print(op)
+
+# set1={"apple","banana","orange","mango"}
+# op="grapes" in set1
+
+# print(op)
+
+# students=["malathi","nani","roshini","anitha","ankith"]
+# op="NANI" not in students
+# print(op)
+
+a=1000
+
+b=1000
+
+print(a is b)
+
+#identity operators are used to check id of objects.
+#is---> means both objects having same id
+#is not-->means both objects having different ids
+
