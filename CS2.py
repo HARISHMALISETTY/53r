a466d53a5d2b99b44322f5d5177cd104c02fd3ad
HARISHMALISETTY <malisettyharish@gmail.com>
2025-06-26 15:25:13 +0530

    conditional statements

---
+++ CS2.py
@@ -0,0 +1,91 @@
+#to check whether num i even or odd
+
+# num=12
+# if(num%2==0):
+#     print("even")
+# else:
+#     print("odd")
+
+# num=152548967
+
+# if(num & 1 ==0):
+#     print("even")
+# else:
+#     print("odd")
+
+# ip="123"
+# print(type(ip))
+# op=int(ip) to convert string to integer
+# op=float(ip) to convert string to float
+# print(type(op))
+
+# num=125
+# op=str(num)
+# print(type(op))
+
+# seq="02468"
+# ip=125701
+# cnvrt=str(ip)
+# if(cnvrt[-1] in seq):
+#     print("even")
+# else:
+#     print("odd")
+
+# seq=[0,2,4,6,8]
+# ip=12504
+# cnvrt=str(ip)
+# if (int(cnvrt[-1]) in seq):
+#     print("even")
+# else:
+#     print("odd")
+
+# if(cndition):
+#     print()
+# elif(cnditin):
+#     print()
+# elif(cndition):
+#     prnt()
+# else:
+#     print()
+
+# color="orange"
+# if(color=="red"):
+#     print("please stop")
+# elif(color=="orange"):
+#     print("ready to go/stop")
+# elif(color=="green"):
+#     print("please go")
+# else:
+#     print("invalid colour input")
+
+"scet","cmr","kakateeya","mallareddy"
+
+# eamcetrank---><25000--->cmr\
+# eamcetrank---->25000 - 50000--->kakateeya
+# eamcetrank--->50000-75000---->mallareddy 
+# eamcetrank--->75000-100000--->scet 
+# eamcet-fail--->go and join bcom/bsc  
+# eamcetRank=45000
+# if(eamcetRank<25000):
+#     print("got seat in CMR")
+# elif(eamcetRank<50000):
+#     print("got seat  in kakateeya ")
+# elif(eamcetRank<75000):
+#     print("got seat in Mallareddy")
+# elif(eamcetRank<100000):
+#     print("got seat in scet")
+# else:
+#     print("go and join in bcom/bsc")
+
+is_login=False
+user="admin"
+
+if(is_login):
+    if(user=="admin"):
+        print("welcome to admin")
+    elif(user=="client"):
+        print("welcome to client")
+    else:
+        print("unauthorized user")
+else:
+    print("go and login")
