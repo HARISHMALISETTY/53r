3b19a87613882e21bf0d3ed5f0a55a920fb8af03
HARISHMALISETTY <malisettyharish@gmail.com>
2025-07-03 14:24:28 +0530

    scope modifiers

--- scope.py
+++ scope.py
@@ -19,3 +19,29 @@
 #         x=10 #so here x is having enclosed scope
 #     func2()
 # func1()
+
+# def func1():
+#    global x #declaring x globally
+#    x=10 # assigning value to that variable
+# func1()
+# print(x)
+
+# x=5
+# def sample():
+#     global x
+#     x=10
+# sample()
+# print(x)
+
+#global is a scope modifier to change the scope to global
+x="hello"
+def fun1():
+    x=5
+    def func2():
+    #    nonlocal x
+       x=10
+    func2()
+    print(x)
+fun1()
+print(x)
+#nonlocal is used to change scope from enclosed to local but not global
