b7fa7961a4c7d6600b027ae5313b48ab97720eea
HARISHMALISETTY <malisettyharish@gmail.com>
2025-07-29 15:06:39 +0530

    inheritance

--- multipleinhertance.py
+++ multipleinhertance.py
@@ -1,14 +1,46 @@
-class Parent1:
-    def P1method(self):
-        print("iam a method from parent1")
-class Parent2:
-    def P2method(self):
-        print("iam a method from parent2")
-class Child(Parent1,Parent2):
-    def CMethod(self):
-        print("iam a method from child")
+# class Parent1:
+#     def P1method(self):
+#         print("iam a method from parent1")
+# class Parent2:
+#     def P2method(self):
+#         print("iam a method from parent2")
+# class Child(Parent1,Parent2):
+#     def CMethod(self):
+#         print("iam a method from child")
 
-obj1=Child()
-# obj1.CMethod()
-# obj1.P1method()
-obj1.P2method()
+# obj1=Child()
+# # obj1.CMethod()
+# # obj1.P1method()
+# obj1.P2method()
+
+# Parent class 1
+class Developer:
+    def __init__(self, emp_id, salary):
+        self.emp_id = emp_id
+        self.salary = salary
+
+    def show_details(self):
+        print(f"Employee ID: {self.emp_id}, Salary: {self.salary}")
+
+# Parent class 2
+class Mentor:
+    def courses(self, courses):
+        self.courses = courses
+        print(f"courses: {', '.join(self.courses)}")
+
+# Child class
+class Trainer(Developer, Mentor):
+    def __init__(self, emp_id, salary, courses):
+        super().__init__(emp_id, salary)
+        self.courses = courses
+
+    def teach(self):
+        print(f"Training courses are: {self.courses}")
+
+# Create object
+Trainer1 = Trainer("T102", 50000, ["frontend","backend","database"])
+
+# Accessing methods from both parent classes
+Trainer1.show_details()         # from Employee
+Trainer1.courses(["html", "python"])  # from Mentor
+Trainer1.teach()                # from Teacher
--- singleinheritance.py
+++ singleinheritance.py
@@ -1,32 +1,53 @@
-class User:
-    def __init__(self,name,email):
-        self.name=name
-        self.email=email
-class Student(User):
-    def __init__(self,name,email,enrolledcourses):
-        super().__init__(name,email)
-        self.enrolledcourses=enrolledcourses
-    def getCourses(self):
-        print(f"{self.name} is learning {self.enrolledcourses}")
-    def removeCourse(self,course):
-        self.enrolledcourses.remove(course)
-        self.getCourses()
-    def addCourse(self,course):
-        self.enrolledcourses.append(course)
-        self.getCourses()
+# class User:
+#     def __init__(self,name,email):
+#         self.name=name
+#         self.email=email
+# class Student(User):
+#     def __init__(self,name,email,enrolledcourses):
+#         super().__init__(name,email)
+#         self.enrolledcourses=enrolledcourses
+#     def getCourses(self):
+#         print(f"{self.name} is learning {self.enrolledcourses}")
+#     def removeCourse(self,course):
+#         self.enrolledcourses.remove(course)
+#         self.getCourses()
+#     def addCourse(self,course):
+#         self.enrolledcourses.append(course)
+#         self.getCourses()
 
-class Instructor(User):
-    def __init__(self,name,email,courses_training):
-        super().__init__(name,email)
-        self.courses_training=courses_training
-    def getCourses(self):
-        print(f"{self.name} is teaching {self.courses_training}")
+# class Instructor(User):
+#     def __init__(self,name,email,courses_training):
+#         super().__init__(name,email)
+#         self.courses_training=courses_training
+#     def getCourses(self):
+#         print(f"{self.name} is teaching {self.courses_training}")
 
-Student_obj=Student("shanmukh","shanmukh@gmail.com",["html","css","python","js"])
-Student_obj.removeCourse("html")
-Student_obj.addCourse("sql")
-# Student_obj.getCourses()
-# Trainer_obj=Instructor("Harish","harish@gmail.com",["frontend","backend","db","AI"])
-# Trainer_obj.getCourses()
+# Student_obj=Student("shanmukh","shanmukh@gmail.com",["html","css","python","js"])
+# Student_obj.removeCourse("html")
+# Student_obj.addCourse("sql")
+# # Student_obj.getCourses()
+# # Trainer_obj=Instructor("Harish","harish@gmail.com",["frontend","backend","db","AI"])
+# # Trainer_obj.getCourses()
 
 
+class ParentActor:
+    def __init__(self,name,Pworth):
+        self.Pname=name
+        self.Pworth=Pworth
+        print(f"{self.Pname} is the parent")
+    def Passets(self):
+        print(f"{self.Pname} assets are {self.Pworth} cr")
+class ChildActor(ParentActor):    
+    def __init__(self,Pname,Cname,Pworth):
+        super().__init__(Pname,Pworth)
+        self.Cname=Cname
+        print(f"{self.Cname}is came by the referrence of{self.Pname}")
+    def Cassets(self,worth):
+        self.Cworth=worth
+        print(f"{self.Cname}is having worth of {self.Cworth}cr")
+    def TotalAssets(self):
+        print(f"total assets of {self.Cname} is {self.Pworth+self.Cworth}")
+ramcharan=ChildActor("chiranjeevi","ramcharan",100)
+ramcharan.Cassets(75)
+ramcharan.TotalAssets()
+ramcharan.Passets()
