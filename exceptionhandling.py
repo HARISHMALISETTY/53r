356801e8c2eaf547ceea8939da55e232631e3379
HARISHMALISETTY <malisettyharish@gmail.com>
2025-09-16 17:20:25 +0530

    intro to exceptional handling

---
+++ exceptionhandling.py
@@ -0,0 +1,33 @@
+#two types of errors--->compile time  and runtime.
+#errors which occurs during compilation due to syntax errors mainly
+#can be considered as compiletime errors
+
+#errors which occurs during runtime/executiontime due to inputs
+#from the user can be considered as runtime errors.
+
+
+#compiletime errors we need to resolve by writing code without
+#any syntax mistakes.
+
+#runtime errors we have to handle by using some techniques like
+#exception handling.
+
+#try and except blocks.
+
+
+# if True:
+#     print("hello world")
+
+
+try:
+    x=4
+    y=5
+    sum=x+y
+    print(sum)
+except:
+    print('error occured')
+
+
+
+
+
